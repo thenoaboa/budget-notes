b1da274b7bf9b27d98e5c6cc5fae26507e9a264a
thenoaboa <ngsatx@gmail.com>
2026-05-25 11:05:13 -0500

    button highlight test

--- src/app/budget/[id]/items.tsx
+++ src/app/budget/[id]/items.tsx
@@ -101,8 +101,6 @@ const styles = StyleSheet.create({
     borderRadius: 14,
     paddingVertical: 13,
     alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#2ECC71",
     marginBottom: 14,
   },
 
