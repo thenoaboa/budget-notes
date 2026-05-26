2baf52c7e9582216ce2234726f9266e1d778c300
thenoaboa <ngsatx@gmail.com>
2026-05-26 14:56:50 -0500

    Add Sentry integration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,14 @@
+import * as Sentry from "@sentry/react-native";
 import { Stack } from "expo-router";
 import { PostHogProvider } from "posthog-react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: "https://e3da9e67549fcae6a7e12ea2426bf8b8@o4511457882472448.ingest.us.sentry.io/4511457890795520",
+  tracesSampleRate: 0.1,
+});
+
+function RootLayout() {
   return (
     <PostHogProvider
       apiKey="phc_x7DdCANjZVq5N2vLrm4mGF3GMVRqgDaF7chXnfHeLrgg"
@@ -20,3 +26,5 @@ export default function RootLayout() {
     </PostHogProvider>
   );
 }
+
+export default Sentry.wrap(RootLayout);
