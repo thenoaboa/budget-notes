c43ab1100a984db0f6b34beeae7033eabdc66a41
thenoaboa <ngsatx@gmail.com>
2026-05-26 13:06:38 -0500

    posthog setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,22 @@
 import { Stack } from "expo-router";
+import { PostHogProvider } from "posthog-react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </GestureHandlerRootView>
+    <PostHogProvider
+      apiKey="phc_x7DdCANjZVq5N2vLrm4mGF3GMVRqgDaF7chXnfHeLrgg"
+      options={{
+        host: "https://us.i.posthog.com",
+      }}
+    >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+      </GestureHandlerRootView>
+    </PostHogProvider>
   );
 }
