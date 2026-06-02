c543f74ce5f53293375c9a7b812b33f05ff3ff80
thenoaboa <ngsatx@gmail.com>
2026-06-02 13:36:12 -0500

    adjust

--- src/components/BudgetHeaderCard.tsx
+++ src/components/BudgetHeaderCard.tsx
@@ -155,7 +155,7 @@ const styles = StyleSheet.create({
 
   dropdownArrow: {
     alignSelf: "flex-end",
-    marginRight: 16,
+    marginRight: 12,
     width: 0,
     height: 0,
     borderLeftWidth: 9,
