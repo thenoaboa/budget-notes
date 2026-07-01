b998a00c48322d6286164c8cbc4b7ab4fb497ae0
thenoaboa <ngsatx@gmail.com>
2026-07-01 16:20:32 -0500

    excluded item opacity

--- src/components/SpendingItemCard.tsx
+++ src/components/SpendingItemCard.tsx
@@ -184,11 +184,11 @@ const styles = StyleSheet.create({
   },
 
   excludedField: {
-    opacity: 0.45,
+    opacity: 0.7,
   },
 
   excludedText: {
-    opacity: 0.65,
+    opacity: 0.8,
   },
 
   itemNameInput: {
